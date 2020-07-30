b155d6ac6657f7232ffb83de882f977d5976b8fb
Zihan Chen <vczh@163.com>
2020-07-29 23:50:19 -0700

    ...

--- Source/Parsing/Json/ParsingJson.h
+++ Source/Parsing/Json/ParsingJson.h
@@ -18,7 +18,15 @@ namespace vl
 		{
 			extern void						JsonEscapeString(const WString& text, stream::TextWriter& writer);
 			extern void						JsonUnescapeString(const WString& text, stream::TextWriter& writer);
+
+			/// <summary>Serialize JSON to string.</summary>
+			/// <param name="node">The JSON node to serialize.</param>
+			/// <param name="writer">The text writer to receive the string.</param>
 			extern void						JsonPrint(Ptr<JsonNode> node, stream::TextWriter& writer);
+
+			/// <summary>Serialize JSON to string.</summary>
+			/// <returns>The serialized string.</returns>
+			/// <param name="node">The JSON node to serialize.</param>
 			extern WString					JsonToString(Ptr<JsonNode> node);
 		}
 	}
--- Source/Parsing/Xml/ParsingXml.h
+++ Source/Parsing/Xml/ParsingXml.h
@@ -22,14 +22,48 @@ namespace vl
 			extern WString							XmlUnescapeCData(const WString& value);
 			extern WString							XmlEscapeComment(const WString& value);
 			extern WString							XmlUnescapeComment(const WString& value);
+
+			/// <summary>Serialize XML to string.</summary>
+			/// <param name="node">The XML node to serialize.</param>
+			/// <param name="writer">The text writer to receive the string.</param>
 			extern void								XmlPrint(Ptr<XmlNode> node, stream::TextWriter& writer);
+
+			/// <summary>Serialize sub nodes in an XML element to string.</summary>
+			/// <param name="element">The XML element in which sub nodes are to be serialized.</param>
+			/// <param name="writer">The text writer to receive the string.</param>
 			extern void								XmlPrintContent(Ptr<XmlElement> element, stream::TextWriter& writer);
+
+			/// <summary>Serialize XML to string.</summary>
+			/// <returns>The serialized string.</returns>
+			/// <param name="node">The XML node to serialize.</param>
 			extern WString							XmlToString(Ptr<XmlNode> node);
 
+			/// <summary>Try to read an attribute in an XML element.</summary>
+			/// <returns>The expected attribute. Returns null if it doesn't exist.</returns>
+			/// <param name="element">The element to find the attribute.</param>
+			/// <param name="name">The name of the attribute.</param>
 			extern Ptr<XmlAttribute>							XmlGetAttribute(Ptr<XmlElement> element, const WString& name);
+
+			/// <summary>Try to read a sub element in an XML element.</summary>
+			/// <returns>The expected sub element. Returns null if it doesn't exist. If there are multiple elements of the expected name, returns the first one.</returns>
+			/// <param name="element">The element to find the sub element.</param>
+			/// <param name="name">The name of the sub element.</param>
 			extern Ptr<XmlElement>								XmlGetElement(Ptr<XmlElement> element, const WString& name);
+
+			/// <summary>Get all sub elements in an XML element.</summary>
+			/// <returns>All sub elements in an XML element.</returns>
+			/// <param name="element">The container XML element.</param>
 			extern collections::LazyList<Ptr<XmlElement>>		XmlGetElements(Ptr<XmlElement> element);
+
+			/// <summary>Try to read sub elements in an XML element.</summary>
+			/// <returns>Expected sub elements. All sub elements of the expected name will be returned.</returns>
+			/// <param name="element">The element to find sub elements.</param>
+			/// <param name="name">The name of sub elements.</param>
 			extern collections::LazyList<Ptr<XmlElement>>		XmlGetElements(Ptr<XmlElement> element, const WString& name);
+
+			/// <summary>Serialize contents in an XML element.</summary>
+			/// <returns>The serialized contents in an XML element.</returns>
+			/// <param name="element">The XML element in which contents are to be serialized.</param>
 			extern WString										XmlGetValue(Ptr<XmlElement> element);
 
 			extern Ptr<XmlAttribute>							XmlGetAttribute(XmlElement* element, const WString& name);
