9357043249b5053dae1b4abbbf96274b68f84122
vczh <vczh@outlook.com>
2021-10-18 02:25:10 -0700

    ...

--- Test/Linux/Main.cpp
+++ Test/Linux/Main.cpp
@@ -18,7 +18,7 @@ TEST_FILE
 	TEST_CASE_ASSERT(Folder(GetTestOutputPath()).Exists());
 }
 
-int main(int argc, char_t* argv[])
+int main(int argc, char* argv[])
 {
 	int result = unittest::UnitTest::RunAndDisposeTests(argc, argv);
 	FinalizeGlobalStorage();
